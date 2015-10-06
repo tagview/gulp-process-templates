d3ab6ad67552d5809d54b3f358decb1536b3e577
Rodrigo Navarro <rnavarro1@gmail.com>
2015-10-06 12:03:58 -0300

    Cleaning up temporary files after running tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 'use strict'
 
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var processTemplates = require('./index.js');
 
 gulp.task('compile', function() {
-  gulp.src('./test/*.js')
-   .pipe(processTemplates())
-   .pipe(gulp.dest('./compiledtest'));
+  return gulp
+    .src('./test/*.js')
+    .pipe(processTemplates())
+    .pipe(gulp.dest('./compiledtest'));
+});
+
+gulp.task('clean', function() {
+  return gulp
+    .src('./compiledtest', { read: false })
+    .pipe(clean({ force: true }));
 });
